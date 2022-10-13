47d5739ba821081e8dd0b100615367ff526d9b68
eblyncho <eblyn52@naver.com>
2022-10-13 16:10:31 +0900

    예외처리 전 오류가 발생하는 코드 사용 시

---
+++ src/ExceptionEx.java
@@ -0,0 +1,24 @@
+public class ExceptionEx {
+    public static void main(String[] args) {
+////        NullPointerException 발생
+//        String data = null;
+//        System.out.println(data.toString());
+//
+////        ArrayIndexOutOfBoundsException 발생
+//        String data1 = args[0];
+//        String data2 = args[1];  // index 값 없음  // 구성 편집으로 프로그램 인수 추가 가능
+//
+//        System.out.println("args[0] : " + data1);
+//        System.out.println("args[1] : " + data2);
+//
+////        NumberFormatException 발생
+//        String data1 = "100";
+//        String data2 = "a100";  //
+//
+//        int value1 = Integer.parseInt(data1);
+//        int value2 = Integer.parseInt(data2);  // Integer.parseInt : 문자열의 숫자부분을 정수로 변환, a100은 오류
+//
+//        int result = value1 + value2;
+//        System.out.println(data1 + " + " + data2 + " = " + result);
+    }
+}
